13af587daf2ad243f6dc215904701a24e3147bcd
Terobau1279 <johnwho864@gmail.com>
2024-09-19 22:15:11 +0545

    Update SignUp.tsx

--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -79,24 +79,7 @@ const SignUp: FunctionComponent<SignUpProps> = ({ setIsShowSignInBox }) => {
             </div>
           </div>
 
-          <div className="flex gap-4 mb-8">
-            <button
-              onClick={() =>
-                signInWithProvider(new GoogleAuthProvider(), "google")
-              }
-              className="h-12 w-12 rounded-full bg-white tw-flex-center hover:brightness-75 transition duration-300"
-            >
-              <FcGoogle size={25} className="text-primary" />
-            </button>
-            <button
-              onClick={() =>
-                signInWithProvider(new FacebookAuthProvider(), "facebook")
-              }
-              className="h-12 w-12 rounded-full bg-white tw-flex-center hover:brightness-75 transition duration-300"
-            >
-              <FaFacebookF size={25} className="text-primary" />
-            </button>
-          </div>
+          
 
           <p className="text-lg">or use your email account: </p>
         </div>
