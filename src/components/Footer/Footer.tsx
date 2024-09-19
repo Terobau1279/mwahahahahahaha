bbb927eefaec36090377423a8b177315c239178c
Terobau1279 <johnwho864@gmail.com>
2024-09-19 21:25:28 +0545

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,9 @@
 import { FunctionComponent } from "react";
-import { AiFillGithub } from "react-icons/ai";
-import { BsFacebook } from "react-icons/bs";
 
 interface FooterProps {}
 
 const Footer: FunctionComponent<FooterProps> = () => {
-  return (
-    
-  );
+  return null;
 };
 
 export default Footer;
